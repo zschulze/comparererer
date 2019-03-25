efd865c4d08d266d385726e54b6c1cf5c03facf5
zschulze <zschulze@att.net>
2019-03-24 23:07:12 -0500

    Ininital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'comparererer';
+
+  form = this.formBuilder.group({
+    inputOne: this.formBuilder.group({
+      port: ['', Validators.required],
+      endpoint: ['', Validators.required]
+    }),
+    inputTwo: this.formBuilder.group({
+      port: ['', Validators.required],
+      endpoint: ['', Validators.required]
+    })
+  });
+
+  files = [];
+  fileResponses = [];
+  inputOneText;
+  inputTwoText;
+  loading;
+  input1;
+  input2;
+
+  constructor(private formBuilder: FormBuilder,
+              private http: HttpClient) { }
+
+  onSubmit() {
+    console.warn(this.form.value);
+    this.inputOneText = 'Hello world';
+    this.inputTwoText = 'Hello World';
+
+    this.loading = true;
+
+    for (const file of this.files) {
+      // this.http.get('http://api.plos.org/search?q=title:DNA')
+      //   .subscribe(res => {
+      //     this.fileResponses.push(
+      //       {
+      //         fileName: file.name,
+      //         inputOneResponse: res,
+      //         inputTwoResponse: res
+      //       }
+      //     );
+      //   }, err => {
+      //
+      //   }, () => {
+      //     this.loading = false;
+      // });
+      this.fileResponses.push(
+        {
+          fileName: file.name,
+          inputOneResponse: this.input1,
+          inputTwoResponse: this.input2
+        }
+      );
+    }
+  }
+
+  fileChange(event) {
+    if (event.target.files.length > 0) {
+      for (const file of event.target.files) {
+        this.files.push(file);
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DiffMatchPatchModule } from 'ng-diff-match-patch';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DiffMatchPatchModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
